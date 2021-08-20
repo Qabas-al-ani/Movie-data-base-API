a87c340fca1debe95057ba7d591f4097e68789b5
Qabas al ani <doctorqa@yahoo.com>
2021-08-20 09:59:14 -0400

    i added POST method that will add new movie to the list of the api whenever we have new movie

--- server.js
+++ server.js
@@ -25,12 +25,25 @@ db.connect(err => {
 
 app.get("/api/movies", (req, res) => {
   db.query("SELECT * FROM movies", function (err, results) {
-    if(err) {
+    if (err) {
       console, log(err);
     } else {
       res.json(results);
     }
   });
+});
+
+app.post("/api/add-movie",(req, res) => {
+  db.query(
+    "INSERT INTO movies (movie_name) VALUES (?)", req.body.movie,
+    function (err, results) {
+      if (err) {
+        console, log(err);
+      } else {
+        res.json(`This movie ${req.body.movie.toUpperCase()} was added`);
+      }
+    }
+  );
 });
 
 app.use((req, res) => {
