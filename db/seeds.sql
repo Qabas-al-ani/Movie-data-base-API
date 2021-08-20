460a11ed25815c28742d9a0e9ae38349b15b9bd1
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 21:57:21 -0400

    i have created the seeds.sql with two tables for movies and reviews

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (movie_name) 
+VALUES('CODA'),
+    ('JUNGLE CRUISE'),
+    ("BECKETT"),
+    ('BLACK WIDOW'),
+    ('THE BOY BEHIND THE DOOR');
+
+
+    INSERT INTO reviews (review) 
+    VALUES ('
+9h ago
+The story did a great job showing the struggles of a deaf family. The lead character also signs and sings. Her family is deaf but their support is tangible.'),
+('The movie was great was funny and exciting to watch would definitely watch the movie again'),
+('The main character’s acting was overly dramatic. I honestly thought the first 20% of the movie was decent and then why lost me when it started getting political. I kept watching but wish I had not. I still don’t fully understand the premise.'),
+('It was absolutely amazing! Felt like I was watching the continuation of the Civil War and before Infinity War! I was really impressed how the story seemed to flow with the other Marvel Movies!'),
+('This was absolutely nail-biting! No gore, all suspense. The usual protagonist poor judgement actually makes sense here. :)');
