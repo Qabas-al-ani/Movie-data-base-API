898d3f2f823428794aeade1f8303fd420ca09f93
Qabas al ani <doctorqa@yahoo.com>
2021-08-20 11:20:22 -0400

    added comment to each line and method to discribe the functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// importing mysql
 const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,7 @@ const db = mysql.createConnection({
   password: "0000",
   database: "movie_db",
 });
-
+// connected to the database
 db.connect(err => {
   if (err) {
     console, log("error");
@@ -23,6 +24,7 @@ db.connect(err => {
   }
 });
 
+// GET method that will select all movies in data base and return the results as json
 app.get("/api/movies", (req, res) => {
   db.query("SELECT * FROM movies", function (err, results) {
     if (err) {
@@ -33,6 +35,7 @@ app.get("/api/movies", (req, res) => {
   });
 });
 
+// POST method that will add new movie to the list in database
 app.post("/api/add-movie", (req, res) => {
   db.query(
     "INSERT INTO movies (movie_name) VALUES (?)",
@@ -47,6 +50,7 @@ app.post("/api/add-movie", (req, res) => {
   );
 });
 
+// POST method that will add new review
 app.post("/api/update-review", (req, res) => {
   db.query(
     "UPDATE reviews SET review = ? WHERE id = ?",
@@ -61,27 +65,24 @@ app.post("/api/update-review", (req, res) => {
   );
 });
 
+// DELETE method that will delete movie with the right id
 app.delete("/api/movie/:id", (req, res) => {
   const { id } = req.params;
-  db.query(
-    "DELETE FROM movies WHERE id = ?",
-    id ,
-    function (err, results) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(`This movie ${id} was deleted`);
-      }
+  db.query("DELETE FROM movies WHERE id = ?", id, function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(`This movie ${id} was deleted`);
     }
-  );
+  });
 });
 
-
-
+// if there is no response show the error message 404
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// listen to PORT
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
